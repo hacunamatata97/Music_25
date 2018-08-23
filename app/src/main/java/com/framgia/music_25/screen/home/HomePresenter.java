fea22f2e99c0ec9dcc4c7196d6c22ee3f87f4c87
DATDT-TT-D1\BadassPsycho <qwerty.lynkaa@gmail.com>
2018-08-23 10:58:50 +0700

    Init Player Bottom View
    Init Player Bottom View - remove redundant method
    
    Init Player Bottom View - fix hard code
    
    Init Player Bottom View - fix hard code
    
    Init Player Bottom View - fix hard code

--- app/src/main/java/com/framgia/music_25/screen/home/HomePresenter.java
+++ app/src/main/java/com/framgia/music_25/screen/home/HomePresenter.java
@@ -1,19 +1,22 @@
 package com.framgia.music_25.screen.home;
 
-public class HomePresenter implements HomeContract.Presenter {
+/**
+ * Listens to user actions from the UI ({@link HomeFragment}), retrieves the data and updates
+ * the UI as required.
+ */
+final class HomePresenter implements HomeContract.Presenter {
 
-    @Override
-    public void setView(HomeContract.View view) {
+    private HomeContract.View mView;
 
+    public HomePresenter(HomeContract.View view) {
+        mView = view;
     }
 
     @Override
     public void onStart() {
-
     }
 
     @Override
     public void onStop() {
-
     }
 }
